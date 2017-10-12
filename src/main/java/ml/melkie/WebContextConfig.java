9c49f253ceb62e8ec441c2ee7725860d157938c5
Richard <1029739550@qq.com>
2017-10-12 23:37:53 +1100

    fix

--- src/main/java/ml/melkie/WebContextConfig.java
+++ src/main/java/ml/melkie/WebContextConfig.java
@@ -19,34 +19,34 @@ import java.net.URISyntaxException;
 public class WebContextConfig {
 
 //    heroku clear db datasource. used in deployment
-//    @Bean
-//    public BasicDataSource getDataSource() throws URISyntaxException {
-//
-//        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-//
-//        String username = dbUri.getUserInfo().split(":")[0];
-//        String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-//
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//
-//        return basicDataSource;
-//    }
+    @Bean
+    public BasicDataSource getDataSource() throws URISyntaxException {
+
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 
 //    localhost mysql datasoure, used in localhost testing
-    @Bean
-    public DataSource getDataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-//        database connection settings
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/Melkie");
-        dataSource.setUsername("root");
-        dataSource.setPassword("richard0625");
-        return dataSource;
-    }
+//    @Bean
+//    public DataSource getDataSource() {
+//        BasicDataSource dataSource = new BasicDataSource();
+////        database connection settings
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/Melkie");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("richard0625");
+//        return dataSource;
+//    }
 
     //transaction management
     @Bean
